c3c574a4425bf129e82767e4e06c1b57aec2374c
DrFerb0902 <trandangkhoa7147@gmail.com>
2026-03-24 23:37:11 +0700

    feat: Hoàn thiện module Quản lý Ngân sách và logic chọn ngày

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import ReceiptScanner from "./pages/ReceiptScanner";
+import Budgets from "./pages/Budgets";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/receipt-scanner" element={<ReceiptScanner />} />
+          <Route path="/budgets" element={<Budgets />} />
         </Route>
       </Routes>
     </BrowserRouter>
